e1d8b2aa1a710d0b032c01ade3668a4153c5b447
SoumyaGupta01 <soumyag2002@gmail.com>
2022-04-01 20:47:38 +0530

    Update

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,7 +3,10 @@ import Layout from "../components/Layout/Layout"
 
 const Contact = () => {
   return (
-  <Layout>Contact
+  <Layout>
+    <div className="container py-5">
+        <h2 className="pb-2 border-bottom text-center">Contact</h2>{" "}
+      </div>
   </Layout>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,15 @@
 import React from "react"
+import Features from "../components/Features/Features"
+import Hero from "../components/Hero/Hero"
 import Layout from "../components/Layout/Layout"
+import Team from "../components/Team/Team"
 
 const Index = () => {
   return (
     <Layout>
-    <div className="card shadow m-2 p-2 text-primary">
-      Test<div className="btn btn-warning">OK</div>
-    </div>
+    <Hero/>
+    <Features/>
+    <Team/>
     </Layout>
   )
 }
